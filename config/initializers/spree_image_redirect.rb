402813fefa7e9bb6da2f207a482b01cb7190e81e
Rudi <rudi@rudikhoury.com>
2025-10-10 11:02:32 +1300

    Override ProxyController to redirect to R2 instead of proxying

--- config/initializers/spree_image_redirect.rb
+++ config/initializers/spree_image_redirect.rb
@@ -2,14 +2,18 @@
 # This ensures images are served directly from R2 instead of proxied through Rails
 
 Rails.application.config.to_prepare do
-  # Override ActiveStorage variant URL generation
-  module ActiveStorageRedirectOverride
-    def url(**options)
-      # For redirect mode, use service_url directly with expiring signature
-      service_url(**options)
+  # Override the proxy route to redirect instead
+  module ActiveStorage
+    module Representations
+      class ProxyController < BaseController
+        include ActiveStorage::SetBlob
+
+        def show
+          # Instead of proxying, redirect to the service URL
+          expires_in ActiveStorage.service_urls_expire_in
+          redirect_to @representation.url(disposition: params[:disposition]), allow_other_host: true
+        end
+      end
     end
   end
-
-  # Apply to all variant types
-  ActiveStorage::VariantWithRecord.prepend(ActiveStorageRedirectOverride) if defined?(ActiveStorage::VariantWithRecord)
 end
